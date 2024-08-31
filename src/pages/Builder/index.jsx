cf95ecf4fdd35b80caf68d4ff34bf51481ad7348
Stefan Syrett <stefansyrett17@gmail.com>
2024-08-31 16:26:01 +0100

    fuck craft.js

--- src/pages/Builder/index.jsx
+++ src/pages/Builder/index.jsx
@@ -4,7 +4,6 @@ import { usePageContext } from "../../contexts/PageContext";
 import { BuilderTabs, BuilderToolBar, Text, Container } from "../../components";
 
 import styles from "./index.module.css";
-import { Editor, Element, Frame } from "@craftjs/core";
 
 export default function Builder() {
   const { pageIndex, activePage, pages, setActivePage } = usePageContext();
@@ -15,33 +14,21 @@ export default function Builder() {
   };
 
   return (
-    <Editor resolver={{ Text, Container }}>
-      <section>
-        <BuilderTabs tabs={pageIndex} activeTab={activePage} handleTabClick={handleTabClick} />
-        <BuilderToolBar screensize={{ scale: 100, width: 1440 }} />
+    <section>
+      <BuilderTabs tabs={pageIndex} activeTab={activePage} handleTabClick={handleTabClick} />
+      <BuilderToolBar screensize={{ scale: 100, width: 1440 }} />
 
-        <div className={styles['builder']}>
+      <div className={styles['builder']}>
 
-          <div className={styles['editor']}></div>
+        <div className={styles['editor']}></div>
 
-          <div className={styles['constructor']}>
-            {activePageData}
-            
-            <Frame>
-              <Element is={Container} padding={4} canvas>
-                
-                <Text fontSize={"20px"} text={"AHH"}/>
-                <Text fontSize={"20px"} text={"WEE"}/>
-                
-              </Element>
-            </Frame>
-          </div>
-
-          <div className={styles['components']}></div>
-
+        <div className={styles['constructor']}>
+          {activePageData}
         </div>
 
-      </section>
-    </Editor>
+        <div className={styles['components']}></div>
+
+      </div>
+    </section>
   );
 }
