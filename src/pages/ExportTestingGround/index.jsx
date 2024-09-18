7ad90481823fcd9e4aa7fd1526b6c8f5ec91807c
Stefan Syrett <stefansyrett17@gmail.com>
2024-09-18 12:20:21 +0100

    adding a bit more info to export preview page

--- src/pages/ExportTestingGround/index.jsx
+++ src/pages/ExportTestingGround/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useExportContext } from '../../contexts/ExportContext';
 
 import styles from './index.module.css'
+import { useProjectContext } from '../../contexts/ProjectContext';
+import { usePageContext } from '../../contexts/PageContext';
 
 function ExportTestingGround() {
   const [showCode, setShowCode] = React.useState(false);
   const { exportedData } = useExportContext();
+  const {  activePage, pageIndex } = usePageContext()
 
 
   return (
     <div className={styles["container"]}>
 
       <div className={styles["meta-data"]}>
-        <h1>Export Testing Ground</h1>
+        <h3>Tab Name: {activePage}</h3>
+        <p>Pages {pageIndex.length}</p>
         <p>Export Type: {exportedData.type}</p>
         <button onClick={() => setShowCode(!showCode)}>Toggle Code</button>
       </div>
